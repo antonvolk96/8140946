4e5d68bb370b1f5d75d157f422907d8010229f2a
Anton <antonvolk96@yandex.ru>
2016-12-22 20:39:38 +0300

    Созданы компоненты

---
+++ local/components/mycompany/complaints.employee.view/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS['NAME_INTRANET'] = "Corporate Portal";
+$MESS['NAME_COMPLAINTS'] = "Complaints";
+$MESS['NAME_COMPONENT'] = "Enter the Complaints values ​​employees";
+$MESS['DESCRIPTION_COMPONENT'] = "Form filling Complaints values employees ";
